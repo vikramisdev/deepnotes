dc8d6a61890ca96c5e8fc27f976b7c68ff884a5e
Vikram <vs423502@gmail.com>
2024-10-10 13:05:54 +0530

    update

---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/com/vikram/deepnotes/data/local/NoteDao_Impl.java
@@ -0,0 +1,192 @@
+package com.vikram.deepnotes.data.local;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class NoteDao_Impl implements NoteDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Note> __insertionAdapterOfNote;
+
+  private final EntityDeletionOrUpdateAdapter<Note> __deletionAdapterOfNote;
+
+  private final EntityDeletionOrUpdateAdapter<Note> __updateAdapterOfNote;
+
+  public NoteDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfNote = new EntityInsertionAdapter<Note>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `note_table` (`id`,`title`,`content`) VALUES (nullif(?, 0),?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Note value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getTitle() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getTitle());
+        }
+        if (value.getContent() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getContent());
+        }
+      }
+    };
+    this.__deletionAdapterOfNote = new EntityDeletionOrUpdateAdapter<Note>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `note_table` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Note value) {
+        stmt.bindLong(1, value.getId());
+      }
+    };
+    this.__updateAdapterOfNote = new EntityDeletionOrUpdateAdapter<Note>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `note_table` SET `id` = ?,`title` = ?,`content` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Note value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getTitle() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getTitle());
+        }
+        if (value.getContent() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getContent());
+        }
+        stmt.bindLong(4, value.getId());
+      }
+    };
+  }
+
+  @Override
+  public Object insert(final Note user, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfNote.insert(user);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object delete(final Note user, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __deletionAdapterOfNote.handle(user);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object update(final Note note, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __updateAdapterOfNote.handle(note);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object getAllNotes(final Continuation<? super List<Note>> continuation) {
+    final String _sql = "SELECT * FROM note_table";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<List<Note>>() {
+      @Override
+      public List<Note> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfContent = CursorUtil.getColumnIndexOrThrow(_cursor, "content");
+          final List<Note> _result = new ArrayList<Note>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final Note _item;
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            final String _tmpTitle;
+            if (_cursor.isNull(_cursorIndexOfTitle)) {
+              _tmpTitle = null;
+            } else {
+              _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            }
+            final String _tmpContent;
+            if (_cursor.isNull(_cursorIndexOfContent)) {
+              _tmpContent = null;
+            } else {
+              _tmpContent = _cursor.getString(_cursorIndexOfContent);
+            }
+            _item = new Note(_tmpId,_tmpTitle,_tmpContent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, continuation);
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
